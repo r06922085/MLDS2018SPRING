3a97f90105420e7cd406029fbc3a0eb733fbe3a2
bear987978897 <bear987978897@gmail.com>
2018-05-09 17:27:27 +0800

    modify .sh files for downloading model files from Gitlab

--- hw2/hw2_1/hw2_seq2seq.sh
+++ hw2/hw2_1/hw2_seq2seq.sh
@@ -1,1 +1,6 @@
+mkdir model_file
+wget https://gitlab.com/bear987978897/MLDS/raw/master/hw2/hw2_1/model_file/attention.ckpt.data-00000-of-00001 -P model_file/
+wget https://gitlab.com/bear987978897/MLDS/raw/master/hw2/hw2_1/model_file/attention.ckpt.index -P model_file/
+wget https://gitlab.com/bear987978897/MLDS/raw/master/hw2/hw2_1/model_file/attention.ckpt.meta -P model_file/
+wget https://gitlab.com/bear987978897/MLDS/raw/master/hw2/hw2_1/model_file/checkpoint -P model_file/
 python3 test.py $1 $2
--- hw2/hw2_2/hw2_seq2seq.sh
+++ hw2/hw2_2/hw2_seq2seq.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+mkdir -p train_model_file
+mkdir -p test_model_file
+wget https://gitlab.com/bear987978897/MLDS/raw/master/hw2/hw2_2/test_model_file/chatbot.ckpt.data-00000-of-00001 -P test_model_file/
+wget https://gitlab.com/bear987978897/MLDS/raw/master/hw2/hw2_2/test_model_file/chatbot.ckpt.index -P test_model_file/
+wget https://gitlab.com/bear987978897/MLDS/raw/master/hw2/hw2_2/test_model_file/chatbot.ckpt.meta -P test_model_file/
+wget https://gitlab.com/bear987978897/MLDS/raw/master/hw2/hw2_2/test_model_file/checkpoint -P test_model_file/
 python3 model_seq2seq.py $1 $2
